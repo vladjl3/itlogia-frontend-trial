2e4515228f156140c5325451807d3ddd5d5863c9
Vladislav Gusselnikov <vladjl3@yahoo.com>
2023-02-14 15:50:51 +0600

    feat: add hamburgers lib

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styles: []
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'itlogia-frontend-trial';
+  title = "itlogia-frontend-trial";
+  isActive: boolean = false;
+
+  onClick(): void {
+    this.isActive = !this.isActive;
+  }
 }

28e9fcc8875caff7aa1834aa75baa2ecfe3fb81a
Vladislav Gusselnikov <vladjl3@yahoo.com>
2023-02-14 21:55:03 +0600

    feat: finish layouting products section

---
+++ src/app/modules/main-page/components/products/products.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/modules/main-page/main-page.module.ts
+++ src/app/modules/main-page/main-page.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { MainPageRoutingModule } from './main-page-routing.module';
 import { MainPageComponent } from './main-page.component';
 import { HeroComponent } from './components/hero/hero.component';
+import { ProductsComponent } from './components/products/products.component';
 
 
 @NgModule({
   declarations: [
     MainPageComponent,
-    HeroComponent
+    HeroComponent,
+    ProductsComponent
   ],
   imports: [
     CommonModule,

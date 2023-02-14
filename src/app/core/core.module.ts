22b3794e5681a88806643bb4186412cd13917e90
Vladislav Gusselnikov <vladjl3@yahoo.com>
2023-02-14 17:58:44 +0600

    feat: add core module and its components

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FooterComponent } from "./components/footer/footer.component";
+import { HeaderComponent } from "./components/header/header.component";
+
+@NgModule({
+  declarations: [HeaderComponent, FooterComponent],
+  imports: [CommonModule],
+  exports: [HeaderComponent, FooterComponent],
+})
+export class CoreModule {}

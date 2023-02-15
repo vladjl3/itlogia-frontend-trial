dfc517425a80a6394ab3c36b7db22fdce9d34d9c
Vladislav Gusselnikov <vladjl3@yahoo.com>
2023-02-15 14:16:01 +0600

    feat: show popup window if order sent

--- src/app/modules/main-page/main-page.component.ts
+++ src/app/modules/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
 import { ProductsService } from "@app/services/products.service";
+import { CreateOrderResponse } from "@app/types/create-order-response.interface";
 import { Pizza } from "@app/types/pizza.interface";
 import { Subscription } from "rxjs";
 
@@ -8,23 +9,34 @@ import { Subscription } from "rxjs";
   styleUrls: ["main-page.component.scss"],
 })
 export class MainPageComponent implements OnInit, OnDestroy {
-  constructor(private productsService: ProductsService) {
-  }
+  constructor(private productsService: ProductsService) {}
 
   pizzzaList: Pizza[] = [];
   pizzaSubscription: Subscription = new Subscription();
+
+  popupVisible: boolean = false;
+  responseMessage: string = "";
 
   ngOnInit(): void {
     this.fetchProducts();
   }
 
   ngOnDestroy(): void {
-      this.pizzaSubscription.unsubscribe()
+    this.pizzaSubscription.unsubscribe();
   }
 
   private fetchProducts(): void {
     this.pizzaSubscription = this.productsService.getProducts().subscribe((products) => {
-        this.pizzzaList = products;
-      });
+      this.pizzzaList = products;
+    });
+  }
+
+  onOrderSent(event: CreateOrderResponse): void {
+    this.responseMessage = event.message;
+    this.popupVisible = true;
+  }
+
+  onClose(): void {
+    this.popupVisible = false;
   }
 }
